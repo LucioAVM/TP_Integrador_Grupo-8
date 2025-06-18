d9f0c4592652eeac8eab4f37114399e3d43f25b5
LucioAVM <lucioavm8@gmail.com>
2025-06-18 20:02:37 -0300

    corrección Navbar redirecciones

--- FrontEnd/Public/assets/js/components/navbar.js
+++ FrontEnd/Public/assets/js/components/navbar.js
@@ -2,21 +2,15 @@ export function renderNavbar() {
   return `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#">Inicio</a>
+        <a class="navbar-brand" href="index.html">Inicio</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Productos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Carrito</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Ticket</a>
-            </li>
+            <li class="nav-item"><a class="nav-link" href="producto.html">Productos</a></li>
+            <li class="nav-item"><a class="nav-link" href="carrito.html">Carrito</a></li>
+            <li class="nav-item"><a class="nav-link" href="ticket.html">Ticket</a></li>
             <li class="nav-item">
               <a class="nav-link" href="#">Login Admin</a>
             </li>
