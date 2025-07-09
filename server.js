ac83b0684ecd6a8a1ad61b759de6c67674b1b4dd
LucioAVM <lucioavm8@gmail.com>
2025-07-09 12:56:08 -0300

    Test Error Conexion Azure (Puerto)

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5000;
+/*const PORT = 5000;*/
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('FrontEnd/Public'));
 
